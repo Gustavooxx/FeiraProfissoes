0352bf53c2d0d169d2953970f0e3fa1c6528758e
Gustavooxx <gustaviixx@gmail.com>
2025-08-11 08:50:24 -0300

    comeco

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,29 @@
+import './index.scss'
+
+export default function Site() {
+    return (
+
+        <div className='container-site'>
+            <h1>ola</h1>
+
+            <div className="topoPage">
+                <div className="logofrei">
+
+                    <img src="" alt="" />
+                </div>
+
+                <div className="tituloPrincipal">
+                    <h1></h1>
+                </div>
+
+                <div className="areaRegis">
+                    <button>Login</button>
+                    <button>Admin</button>
+                    
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
