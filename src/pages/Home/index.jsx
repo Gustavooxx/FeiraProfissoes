dc0237d37b61c825f899bad27366f7b7cbc7257a
Gustavooxx <gustaviixx@gmail.com>
2025-08-11 16:24:09 -0300

    pag inicio

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,11 +19,46 @@ export default function Site() {
                 <div className="tituloPrincipal">
                 </div>
 
-                <div className="areaRegis">
-                   <Link>Login</Link>
-                    <Link>Admin</Link>
-                    
+                 <div className="areaRegistro">
+
+                        <Link >Login</Link>
+                        <Link id='admin'>Administrador</Link>
+                        
+                 </div>
+
+
+            </div>
+
+            <div className='descricoes'>
+
+                <div className='texto'>
+
+                    <h2>O FUTURO ESTÁ AQUI</h2>
+
+                    <h3>5 Feira De <br />Profissões </h3>
+
+                    <Link>Cadastre-se Agora</Link>
+
+                    <div className='projetos'>
+
+                        <div className='center'>
+
+                            <div className='images'>
+                                <img src="/assets/images/imagemFrei.png" alt="" />
+                            </div>
+
+                            <div className='texto'>
+
+                            <p>participe de nossos projetos aqui esta alguns do nossos projetos na feira passada</p>
+
+                            </div>
+                            
+                        </div>
+
+                    </div>
+
                 </div>
+
 
             </div>
 
