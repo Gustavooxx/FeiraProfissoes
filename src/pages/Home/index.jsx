2d88575609ec28cbf9580c204f2c5d49435ae310
Gustavooxx <gustaviixx@gmail.com>
2025-08-12 11:12:48 -0300

    arrumado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,9 +16,6 @@ export default function Site() {
                 </div>
 
 
-                <div className="tituloPrincipal">
-                </div>
-
                 <div className="areaRegistro">
 
 
@@ -26,47 +23,55 @@ export default function Site() {
                     <Link id='admin'>Administrador</Link>
 
                 </div>
- 
-                 </div>
+
+            </div>
+
+            <div className='descricoes'>
+
+                <div className='texto'>
 
                     <h2>ESFORÇO QUE TRANSFORMA E O ORGULHO PERMANECE</h2>
 
                     <h3>5 Feira De Profissões </h3>
 
                     <Link to='/Login'>Cadastre-se Agora</Link>
-                <div/>
 
-                            <div className='images'>
-                                <img src="/assets/images/imagemFrei.png" alt="" />
-                            </div>
-
-                            <div className='freiXavier'>
-                                <div className='img-localIntituto'>
-                                    <img src="/assets/images/localFrei.png" width='400px' height='300px'/>
-
-                                <div className=' titulos'>
-                            </div>
-            </div>
-            
-            <div className="imgsAlunos">
-                <img src="/public/assets/images/alunosfrei/alunos1.jpeg" />
-            </div>
-
-
-            <div className="apresentacao">
-
-                <div className="titulo">
-                    <h1>Quem somos nós?</h1>
                 </div>
 
-                <div className="texto">
-                    <p>Fundado por Frei Ambrogio (conhecido como Frei Xavier) entre as décadas de 1960 e 1970, o instituto nasceu como uma resposta às necessidades da comunidade de Veleiros, na zona sul de São Paulo. Integrado à Paróquia de Nossa Senhora de Fátima, começou com uma creche em 1971 e, em 1979, inaugurou a Escola Profissional Nossa Senhora de Fátima, com foco na formação técnica e profissional de jovens e adolescentes carentes da região .</p>
-                </div>
             </div>
 
-        </div>
+            <div className='projetos'>
+
+                <div className='center'>
+
+                    <div className='images'>
+                        <img src="/assets/images/imagemFrei.png" alt="" />
+                    </div>
+
+                    <div className='texto'>
+
+                        <p>participe de nossos projetos aqui esta alguns do nossos projetos na feira passada</p>
+
+                    </div>
+
+                <div className='freiXavier'>
+                        <div className='img-localIntituto'>
+                             <img src="/assets/images/localFrei.png" width='400px' height='300px' />
+
+                        </div>
+                </div>
+                    
+
+                </div>
+
+               
+
+            </div>
+
+
         </div>
 
 
     )
 }
+
