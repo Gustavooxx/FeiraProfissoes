53d1cc41c4da4baf053ee1c7ea0fae1f7f4f7c20
Gustavooxx <gustaviixx@gmail.com>
2025-08-16 12:04:52 -0300

    cadastro

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,45 +3,84 @@ import { Link } from 'react-router-dom'
 
 export default function Login() {
     return (
-
-
         <div>
-
             <div className="container">
+                <form>
+                    <h1>Cadastro de Usuário</h1>
 
-                <form >
-
-                    <h1>Nome</h1>
-
-                    <div className="inputs">
-                        <input placeholder="Email" type='email' />
-                    </div>
-                    <div className="inp">
-                        <input placeholder="Sua senha" type='password' />
-                    </div>
-
-                    <div className='Lembrarsenha'>
-
-                        <label>
-                            <input type="checkbox" />
-                            Lembrar minha senha
-                        </label>
-                        <a href="#">Esqueci a minha senha</a>
-
+                    <div className="form-group">
+                        <label>Nome Completo *</label>
+                        <input 
+                            type="text" 
+                            name="nome" 
+                            placeholder="Digite seu nome completo" 
+                            required 
+                        />
                     </div>
 
-                    <button type='submit'>Login</button>
-
-                    <div className='Cadastrar'>
-                        <p>Não tem cadastro? <a href="#">Cadastre-se</a></p>
+                    <div className="form-group">
+                        <label>Email *</label>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            placeholder="seu@email.com" 
+                            required 
+                        />
                     </div>
+
+                    <div className="form-group">
+                        <label>Senha *</label>
+                        <input 
+                            type="password" 
+                            name="senha" 
+                            placeholder="Digite sua senha" 
+                            required 
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Telefone *</label>
+                        <input 
+                            type="tel" 
+                            name="telefone" 
+                            placeholder="(00) 00000-0000" 
+                            required 
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Data de Nascimento *</label>
+                        <input 
+                            type="date" 
+                            name="data_nascimento" 
+                            required 
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Endereço Completo *</label>
+                        <input 
+                            type="text" 
+                            name="endereco" 
+                            placeholder="Rua, número, bairro, cidade - UF" 
+                            required 
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>CEP *</label>
+                        <input 
+                            type="text" 
+                            name="cep" 
+                            placeholder="00000-000" 
+                            required 
+                        />
+                    </div>
+
+                    <button type='submit'>Cadastrar</button>
+
                 </form>
-
-
             </div>
-
         </div>
-
-
     )
 }
